ccbf02fc13fe4d3e00b7831ccf1e3c77bfe6258b
vicanso <vicansocanbico@gmail.com>
2014-12-14 19:28:01 +0800

    commit

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,13 @@
 "use strict";
 var logger = require('../lib/logger');
 logger.appPath = '/Users/tree/快盘/github/jtlogger/';
+logger.mode = 'file';
+logger.logPath = '/Users/tree/tmp/jtloger';
 
 
-console.log('log %s', 'test');
-console.info('info');
-console.error('error');
-console.warn('warn');
+setInterval(function(){
+  console.log('log %s', 'test');
+  console.info('info');
+  console.error('error');
+  console.warn('warn');
+}, 1000);
